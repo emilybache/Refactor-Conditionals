cb8a035032e011161d256d6953a8bc400c743266
rolger <roland.germ@gmail.com>
2021-07-14 12:05:24 +0200

    style: rearranged order - start with simple cases

--- java/src/main/java/condexample/ExampleConditionals.java
+++ java/src/main/java/condexample/ExampleConditionals.java
@@ -2,29 +2,28 @@ package condexample;
 
 public class ExampleConditionals {
 
-	static boolean deMorgan(int x) {
-		return !(x != 5 && x != 7);
+	static int invert(int x) {
+		if (x != 3) {
+			return 3;
+		} else {
+			return 4;
+		}
 	}
 
-	static int normalize(String s1, String s2) {
-		if (!s1.equals("hello")) {
-			if (!s2.equals("world")) {
-				if (!s1.equals("foo")) {
-					return 6;
-				}
-				return 1;
-			} else if (!s1.equals("foo")) {
-				return 2;
-			}
-		} else {
-			if (s2.equals("bar")) {
-				return 3;
-			} else if (!s2.equals("world")) {
-				return 4;
-			}
-			return 5;
+	static int redundant_else(int x) {
+		if (x < 3) {
+			return 1;
+		} else if (x < 10) {
+			return 10;
+		}
+		if (x < 30) {
+			return 30;
 		}
 		return 0;
+	}
+
+	static boolean deMorgan(int x) {
+		return !(x != 5 && x != 7);
 	}
 
 	static int split(int x, int y) {
@@ -66,24 +65,25 @@ public class ExampleConditionals {
 		return result * factor;
 	}
 
-	static int redundant_else(int x) {
-		if (x < 3) {
-			return 1;
-		} else if (x < 10) {
-			return 10;
-		}
-		if (x < 30) {
-			return 30;
+	static int normalize(String s1, String s2) {
+		if (!s1.equals("hello")) {
+			if (!s2.equals("world")) {
+				if (!s1.equals("foo")) {
+					return 6;
+				}
+				return 1;
+			} else if (!s1.equals("foo")) {
+				return 2;
+			}
+		} else {
+			if (s2.equals("bar")) {
+				return 3;
+			} else if (!s2.equals("world")) {
+				return 4;
+			}
+			return 5;
 		}
 		return 0;
-	}
-
-	static int invert(int x) {
-		if (x != 3) {
-			return 3;
-		} else {
-			return 4;
-		}
 	}
 
 }
