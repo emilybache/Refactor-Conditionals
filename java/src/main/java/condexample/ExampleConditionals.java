130091cedf5d43a387eb94fd55170e3da3c02adf
emily <emily@bacheconsulting.com>
2025-01-22 14:18:22 +0100

    liftUpSimple example

--- java/src/main/java/condexample/ExampleConditionals.java
+++ java/src/main/java/condexample/ExampleConditionals.java
@@ -2,105 +2,138 @@ package condexample;
 
 public class ExampleConditionals {
 
-	static int invert(int x) {
-		if (x != 3) {
-			return 3;
-		} else {
-			return 4;
-		}
-	}
+    static int invert(int x) {
+        if (x != 3) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
 
-	static int redundant_else(int x) {
-		if (x < 3) {
-			return 1;
-		} else if (x < 10) {
-			return 10;
-		}
-		if (x < 30) {
-			return 30;
-		}
-		return 0;
-	}
+    static int redundant_else(int x) {
+        if (x < 3) {
+            return 1;
+        } else if (x < 10) {
+            return 10;
+        }
+        if (x < 30) {
+            return 30;
+        }
+        return 0;
+    }
 
-	static boolean deMorgan(int x) {
-		return !(x != 5 && x != 7);
-	}
+    static boolean deMorgan(int x) {
+        return !(x != 5 && x != 7);
+    }
 
-	static int join_AND(int x, int y) {
-		if (x == 3) {
-			if (y == 4) {
-				return x + y;
-			}
-		}
-		return 0;
-	}
+    static int join_AND(int x, int y) {
+        if (x == 3) {
+            if (y == 4) {
+                return x + y;
+            }
+        }
+        return 0;
+    }
 
-	static int split_AND(int x, int y) {
-		if (x == 3 && y == 4) {
-			return x + y;
-		} else {
-			return 0;
-		}
-	}
+    static int split_AND(int x, int y) {
+        if (x == 3 && y == 4) {
+            return x + y;
+        } else {
+            return 0;
+        }
+    }
 
-	static boolean join_OR(int x, int y) {
-		if (x >= 0) {
-			return true;
-		}
-		if (y <= 3) {
-			return true;
-		}
-		return y == 10;
-	}
+    static boolean join_OR(int x, int y) {
+        if (x >= 0) {
+            return true;
+        }
+        if (y <= 3) {
+            return true;
+        }
+        return y == 10;
+    }
 
-	static boolean split_OR(int x, int y) {
-		if (x >= 0 || y <= 3 || y == 10) {
-			return true;
-		}
-		return false;
-	}
+    static boolean split_OR(int x, int y) {
+        if (x >= 0 || y <= 3 || y == 10) {
+            return true;
+        }
+        return false;
+    }
 
-	static int join_statements(int x, int y) {
-		int result = 0;
-		int factor = 1;
-		if (x > 3) {
-			factor = x;
-		}
-		if (x > 3) {
-			result += y * 3;
-		}
-		return result * factor;
-	}
+    static int join_statements(int x, int y) {
+        int result = 0;
+        int factor = 1;
+        if (x > 3) {
+            factor = x;
+        }
+        if (x > 3) {
+            result += y * 3;
+        }
+        return result * factor;
+    }
 
-	static int split_statements(int x, int y) {
-		int result = 0;
-		int factor = 1;
-		if (x > 3) {
-			result += y * 3;
-			factor = x;
-		}
-		return result * factor;
-	}
+    static int split_statements(int x, int y) {
+        int result = 0;
+        int factor = 1;
+        if (x > 3) {
+            result += y * 3;
+            factor = x;
+        }
+        return result * factor;
+    }
 
-	static int normalize(String s1, String s2) {
-		if (!s1.equals("hello")) {
-			if (!s2.equals("world")) {
-				if (!s1.equals("foo")) {
-					return 6;
-				}
-				return 1;
-			} else if (!s1.equals("foo")) {
-				return 2;
-			}
-		} else {
-			if (s2.equals("bar")) {
-				return 3;
-			} else if (!s2.equals("world")) {
-				return 4;
-			}
-			return 5;
-		}
-		return 0;
-	}
+    public static String liftUpSimple(boolean a, boolean b) {
+        if (a) {
+            if (b) {
+                return aTrueBTrue();
+            } else {
+                return aTrueBFalse();
+            }
+        } else {
+            if (b) {
+                return aFalseBTrue();
+            } else {
+                return aFalseBFalse();
+            }
+        }
+    }
+
+    public static String aFalseBFalse() {
+        return "AFalseBFalse";
+    }
+
+    public static String aFalseBTrue() {
+        return "AFalseBTrue";
+    }
+
+    public static String aTrueBFalse() {
+        return "ATrueBFalse";
+    }
+
+    public static String aTrueBTrue() {
+        return "ATrueBTrue";
+    }
+
+
+    static int normalize(String s1, String s2) {
+        if (!s1.equals("hello")) {
+            if (!s2.equals("world")) {
+                if (!s1.equals("foo")) {
+                    return 6;
+                }
+                return 1;
+            } else if (!s1.equals("foo")) {
+                return 2;
+            }
+        } else {
+            if (s2.equals("bar")) {
+                return 3;
+            } else if (!s2.equals("world")) {
+                return 4;
+            }
+            return 5;
+        }
+        return 0;
+    }
 
 }
--- java/src/test/java/condexample/ExampleConditionalsTest.java
+++ java/src/test/java/condexample/ExampleConditionalsTest.java
@@ -1,128 +1,152 @@
 package condexample;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ExampleConditionalsTest {
 
-	@ParameterizedTest
-	@CsvSource({ 
-		"4, 3", 
-		"3, 5", 
-		"3, 1", 
-		"3, 4" 
-		})
-	void invert(int expected, int actual) {
-		Assertions.assertEquals(expected, ExampleConditionals.invert(actual));
-	}
+    @ParameterizedTest
+    @CsvSource({
+            "4, 3",
+            "3, 5",
+            "3, 1",
+            "3, 4"
+    })
+    void invert(int expected, int actual) {
+        assertEquals(expected, ExampleConditionals.invert(actual));
+    }
 
-	@ParameterizedTest
-	@CsvSource({ 
-		"2, 1", 
-		"7, 10", 
-		"29, 30", 
-		"30, 0" 
-		})
-	void join_statements(int x, int expected) {
-		Assertions.assertEquals(expected, ExampleConditionals.redundant_else(x));
-	}
-	
-	@ParameterizedTest
-	@ValueSource(ints = { 5, 7 })
-	void deMorgan_true(int valid) {
-		Assertions.assertTrue(ExampleConditionals.deMorgan(valid));
-	}
+    @ParameterizedTest
+    @CsvSource({
+            "2, 1",
+            "7, 10",
+            "29, 30",
+            "30, 0"
+    })
+    void join_statements(int x, int expected) {
+        assertEquals(expected, ExampleConditionals.redundant_else(x));
+    }
 
-	@ParameterizedTest
-	@ValueSource(ints = { 4, 8 })
-	void deMorgan_false(int invalid) {
-		Assertions.assertFalse(ExampleConditionals.deMorgan(invalid));
-	}
+    @ParameterizedTest
+    @ValueSource(ints = {5, 7})
+    void deMorgan_true(int valid) {
+        Assertions.assertTrue(ExampleConditionals.deMorgan(valid));
+    }
 
-	@ParameterizedTest
-	@CsvSource({ 
-		"4, 3, 0", 
-		"7, 3, 0", 
-		"1, 3, 0", 
-		"3, 4, 7", 
-		"3, 11, 0" 
-		})
-	void join_AND(int x, int y, int expected) {
-		Assertions.assertEquals(expected, ExampleConditionals.join_AND(x, y));
-	}
-	
-	@ParameterizedTest
-	@CsvSource({ 
-		"4, 3, 0", 
-		"7, 3, 0", 
-		"1, 3, 0", 
-		"3, 4, 7", 
-		"3, 11, 0" 
-		})
-	void split_AND(int x, int y, int expected) {
-		Assertions.assertEquals(expected, ExampleConditionals.split_AND(x, y));
-	}
+    @ParameterizedTest
+    @ValueSource(ints = {4, 8})
+    void deMorgan_false(int invalid) {
+        Assertions.assertFalse(ExampleConditionals.deMorgan(invalid));
+    }
 
-	@ParameterizedTest
-	@CsvSource({ 
-		"0, 3, true", 
-		"3, 6, true", 
-		"-1, 3, true", 
-		"-1, 5, false", 
-		"-1, 10, true"
-		})
-	void join_OR(int x, int y, boolean expected) {
-		Assertions.assertEquals(expected, ExampleConditionals.join_OR(x, y));
-	}
-	
-	@ParameterizedTest
-	@CsvSource({ 
-		"0, 3, true", 
-		"3, 6, true", 
-		"-1, 3, true", 
-		"-1, 5, false", 
-		"-1, 10, true"
-		})
-	void split_OR(int x, int y, boolean expected) {
-		Assertions.assertEquals(expected, ExampleConditionals.split_OR(x, y));
-	}
-	
-	@ParameterizedTest
-	@CsvSource({ 
-		"4, 3, 36", 
-		"7, 3, 63", 
-		"1, 3, 0", 
-		"3, 4, 0" 
-		})
-	void split_statements(int x, int y, int expected) {
-		Assertions.assertEquals(expected, ExampleConditionals.split_statements(x, y));
-	}
+    @ParameterizedTest
+    @CsvSource({
+            "4, 3, 0",
+            "7, 3, 0",
+            "1, 3, 0",
+            "3, 4, 7",
+            "3, 11, 0"
+    })
+    void join_AND(int x, int y, int expected) {
+        assertEquals(expected, ExampleConditionals.join_AND(x, y));
+    }
 
-	@ParameterizedTest
-	@CsvSource({ 
-		"4, 3, 36", 
-		"7, 3, 63", 
-		"1, 3, 0", 
-		"3, 4, 0" 
-		})
-	void join_statements(int x, int y, int expected) {
-		Assertions.assertEquals(expected, ExampleConditionals.join_statements(x, y));
-	}
+    @ParameterizedTest
+    @CsvSource({
+            "4, 3, 0",
+            "7, 3, 0",
+            "1, 3, 0",
+            "3, 4, 7",
+            "3, 11, 0"
+    })
+    void split_AND(int x, int y, int expected) {
+        assertEquals(expected, ExampleConditionals.split_AND(x, y));
+    }
 
-	@ParameterizedTest
-	@CsvSource({ 
-		  "foo,   world, 0",
-		  "foo,   bar,   1",
-		  "bar,   world, 2",
-		  "hello, bar,   3",
-		  "hello, foo,   4",
-		  "hello, world, 5",
-		  "bar,   foo,   6",
-		})
-	void normalize(String s1, String s2, int expected) {
-		Assertions.assertEquals(expected, ExampleConditionals.normalize(s1, s2));
-	}
-	
+    @ParameterizedTest
+    @CsvSource({
+            "0, 3, true",
+            "3, 6, true",
+            "-1, 3, true",
+            "-1, 5, false",
+            "-1, 10, true"
+    })
+    void join_OR(int x, int y, boolean expected) {
+        assertEquals(expected, ExampleConditionals.join_OR(x, y));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 3, true",
+            "3, 6, true",
+            "-1, 3, true",
+            "-1, 5, false",
+            "-1, 10, true"
+    })
+    void split_OR(int x, int y, boolean expected) {
+        assertEquals(expected, ExampleConditionals.split_OR(x, y));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "4, 3, 36",
+            "7, 3, 63",
+            "1, 3, 0",
+            "3, 4, 0"
+    })
+    void split_statements(int x, int y, int expected) {
+        assertEquals(expected, ExampleConditionals.split_statements(x, y));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "4, 3, 36",
+            "7, 3, 63",
+            "1, 3, 0",
+            "3, 4, 0"
+    })
+    void join_statements(int x, int y, int expected) {
+        assertEquals(expected, ExampleConditionals.join_statements(x, y));
+    }
+
+    @Test
+    public void testA_True_B_True() {
+        assertEquals("ATrueBTrue", ExampleConditionals.liftUpSimple(true, true));
+    }
+
+    @Test
+    public void testA_True_B_False() {
+        assertEquals("ATrueBFalse", ExampleConditionals.liftUpSimple(true, false));
+    }
+
+    @Test
+    public void testA_False_B_True() {
+        assertEquals("AFalseBTrue", ExampleConditionals.liftUpSimple(false, true));
+    }
+
+    @Test
+    public void testA_False_B_False() {
+        assertEquals("AFalseBFalse", ExampleConditionals.liftUpSimple(false, false));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "foo,   world, 0",
+            "foo,   bar,   1",
+            "bar,   world, 2",
+            "hello, bar,   3",
+            "hello, foo,   4",
+            "hello, world, 5",
+            "bar,   foo,   6",
+    })
+    void normalize(String s1, String s2, int expected) {
+        assertEquals(expected, ExampleConditionals.normalize(s1, s2));
+    }
+
 }
