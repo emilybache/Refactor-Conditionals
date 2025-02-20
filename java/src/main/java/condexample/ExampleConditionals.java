caca731d17d0d09caeb58ddcec8598afb0e17be8
emily <emily@bacheconsulting.com>
2025-02-20 10:56:49 +0100

    fix one exercise

--- java/src/main/java/condexample/ExampleConditionals.java
+++ java/src/main/java/condexample/ExampleConditionals.java
@@ -3,14 +3,6 @@ package condexample;
 public class ExampleConditionals {
 
     static int invert_negative(int x) {
-        if (x != 3) {
-            return 3;
-        } else {
-            return 4;
-        }
-    }
-
-    static int invert_positive(int x) {
         if (x == 3) {
             return 4;
         } else {
@@ -18,19 +10,15 @@ public class ExampleConditionals {
         }
     }
 
-    static int redundant_else(int x) {
-        if (x < 3) {
-            return 1;
-        } else if (x < 10) {
-            return 10;
+    static int invert_positive(int x) {
+        if (x != 3) {
+            return 3;
+        } else {
+            return 4;
         }
-        if (x < 30) {
-            return 30;
-        }
-        return 0;
     }
 
-    static int missing_else(int x) {
+    static int redundant_else(int x) {
         if (x < 3) {
             return 1;
         }
@@ -43,14 +31,33 @@ public class ExampleConditionals {
         return 0;
     }
 
-    static boolean deMorganAnd(int x) {
-        return !(x != 5 && x != 7);
+    static int missing_else(int x) {
+        if (x < 3) {
+            return 1;
+        } else if (x < 10) {
+            return 10;
+        } else if (x < 30) {
+            return 30;
+        }
+        return 0;
     }
-    static boolean deMorganOr(int x) {
+
+    static boolean deMorganAnd(int x) {
         return x == 5 || x == 7;
     }
 
+    static boolean deMorganOr(int x) {
+        return !(x != 5 && x != 7);
+    }
+
     static int join_AND(int x, int y) {
+        if (x == 3 && y == 4) {
+            return x + y;
+        }
+        return 0;
+    }
+
+    static int split_AND(int x, int y) {
         if (x == 3) {
             if (y == 4) {
                 return x + y;
@@ -59,26 +66,19 @@ public class ExampleConditionals {
         return 0;
     }
 
-    static int split_AND(int x, int y) {
-        if (x == 3 && y == 4) {
-            return x + y;
-        } else {
-            return 0;
-        }
-    }
-
     static boolean join_OR(int x, int y) {
-        if (x >= 0) {
+        if (x >= 0 || y <= 3 || y == 10) {
             return true;
         }
-        if (y <= 3) {
-            return true;
-        }
-        return y == 10;
+        return false;
     }
 
     static boolean split_OR(int x, int y) {
-        if (x >= 0 || y <= 3 || y == 10) {
+        if (x >= 0) {
+            return true;
+        } else if (y <= 3) {
+            return true;
+        } else if (y == 10) {
             return true;
         }
         return false;
@@ -89,8 +89,6 @@ public class ExampleConditionals {
         int factor = 1;
         if (x > 3) {
             factor = x;
-        }
-        if (x > 3) {
             result += y * 3;
         }
         return result * factor;
@@ -101,6 +99,9 @@ public class ExampleConditionals {
         int factor = 1;
         if (x > 3) {
             result += y * 3;
+        }
+        if (x > 3) {
+
             factor = x;
         }
         return result * factor;
@@ -111,14 +112,6 @@ public class ExampleConditionals {
     public static int z;
 
     static void introduce_guard_clause_simple(int x) {
-        if (x == 3) {
-            ExampleConditionals.y = 4;
-            // imagine lots more lines of code here including if statements
-            ExampleConditionals.z = x;
-        }
-    }
-
-    static void remove_guard_clause_simple(int x) {
         if (x != 3) {
             return;
         }
@@ -127,26 +120,36 @@ public class ExampleConditionals {
         ExampleConditionals.z = x;
     }
 
+    static void remove_guard_clause_simple(int x) {
+        if (x == 3) {
+            ExampleConditionals.y = 4;
+            // imagine lots more lines of code here including if statements
+            ExampleConditionals.z = x;
+        }
+    }
+
     static int introduce_guard_clause_single_return(int x) {
         int result = 0;
         if (x != 3) {
             ExampleConditionals.y = 4;
             // imagine lots more lines of code here including if statements
             ExampleConditionals.z = x;
-            result = 4*x + y;
+            result = 4 * x + y;
         }
         return result;
     }
 
     static int remove_guard_clause_single_return(int x) {
+        int result = 0;
         if (x == 3) {
-            return 0;
+            return result;
         }
 
         ExampleConditionals.y = 4;
         // imagine lots more lines of code here including if statements
         ExampleConditionals.z = x;
-        return 4*x + y;
+        result = 4 * x + y;
+        return result;
     }
 
     static int introduce_guard_clause_multi_return(int x) {
